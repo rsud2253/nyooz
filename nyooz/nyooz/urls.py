91e61ccb4171a6025f31a6471473a10cdefc998a
rsud2253 <rsud2253@gmail.com>
2012-07-07 23:44:43 +0530

    work on July 7 NIGHT

--- nyooz/nyooz/urls.py
+++ nyooz/nyooz/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from nyooz_home.models import Local, Home, City
+from nyooz_home.views import get_local
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,5 +15,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    'nyooz.nyooz_home.views',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^nyooz_home/',get_local),
 )
--- nyooz/nyooz_home/views.py
+++ nyooz/nyooz_home/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+from nyooz_home.models import Local
+
+def get_local(request):
+	news=Local.objects.all().order_by('priority')[:5]
+	return	render_to_response('Local/index.html',{'news':news})
+	
